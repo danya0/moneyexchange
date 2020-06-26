f88b204fb1d4fd8722e490847a6637c1b7322087
Данил Самаркин <samarkin.danil@yandex.ru>
2020-06-26 13:28:42 +0300

    сверстал section1

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
 import header from './modules/header';
+import section1 from './modules/section1';
 header();
+section1();
---
+++ src/js/modules/section1.js
@@ -0,0 +1,66 @@
+export default () => {
+	let selects = $('.form-input-block__select'),
+		selectItem = $('.select-item');
+
+	selects.click(function (e) {
+		$(this).toggleClass('select_active');
+		$(this).children('.select__icon').toggleClass('select__icon_active');
+	});
+
+	selectItem.each((index, item) => {
+		let currentIcon = $(item)
+				.parents('.form-input-block__select')
+				.children('.select__header')
+				.children('.select__current')
+				.children('.select-item-current')
+				.children('img'),
+			currentText = $(item)
+				.parents('.form-input-block__select')
+				.children('.select__header')
+				.children('.select__current')
+				.children('.select-item-current')
+				.children('span');
+
+		$(item).click(function (e) {
+			currentIcon.attr('src', $(this).children('img').attr('src'));
+			currentText.text($(this).children('span').text());
+			countValue();
+		});
+	});
+
+	let firstInput = $('.form-input-block:first input'),
+		lastInput = $('.form-input-block:last input');
+	firstInput.change(function (e) {
+		e.preventDefault();
+		countValue();
+	});
+
+	function countValue() {
+		let firstSelect = $('.select-item-current:first span'),
+			lastSelect = $('.select-item-current:last span'),
+			firstInputVal = +firstInput.val(),
+			firstValue = 0,
+			lastValue = 0;
+
+		let rub = 1,
+			usd = 69,
+			eur = 73;
+		if (firstSelect.text() == 'RUB') {
+			firstValue = rub;
+		} else if (firstSelect.text() == 'USD') {
+			firstValue = usd;
+		} else if (firstSelect.text() == 'EUR') {
+			firstValue = eur;
+		}
+		if (lastSelect.text() == 'RUB') {
+			lastValue = rub;
+		} else if (lastSelect.text() == 'USD') {
+			lastValue = usd;
+		} else if (lastSelect.text() == 'EUR') {
+			lastValue = eur;
+		}
+
+		lastInput.val(((firstInputVal / lastValue) * firstValue).toFixed(2));
+	}
+	countValue();
+};
