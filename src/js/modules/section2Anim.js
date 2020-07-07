c4fe875e2712d4abe9c44e063701fa65cb9f0ad0
Данил Самаркин <samarkin.danil@yandex.ru>
2020-07-07 20:13:34 +0300

    пофиксил анимацию карточек section2

--- src/js/modules/section2Anim.js
+++ src/js/modules/section2Anim.js
@@ -2,10 +2,11 @@ export default () => {
 	let images = $('.advantages-block__img');
 
 	$(window).scroll(function () {
-		if ($(window).scrollTop() > 400) {
+		if ($(window).scrollTop() > 200) {
 			$(images).removeClass('unvisible');
 			setTimeout(() => {
 				images.css('transition', '0.5s');
+				images.addClass('img-hover');
 			}, 2500);
 		}
 	});
