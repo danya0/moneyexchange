35bf4283d76b61cd1abd1280590d522e9d54128d
Данил Самаркин <samarkin.danil@yandex.ru>
2020-07-09 11:08:15 +0300

    изменил структуру js

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,10 @@ import section4 from './modules/section4';
 import section1Anim from './modules/section1Anim';
 import section2Anim from './modules/section2Anim';
 
-window.addEventListener('DOMContentLoaded', ()=> {
-  header();
-  section1();
-  section4();
-  section1Anim();
-  section2Anim();
-})
+window.addEventListener('DOMContentLoaded', () => {
+	header();
+	section1();
+	section4();
+	section1Anim();
+	section2Anim();
+});
